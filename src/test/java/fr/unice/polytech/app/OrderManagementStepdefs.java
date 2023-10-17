fa7167c3a8a6ded0b925beb3d8cbb4a17764afaf
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-10-17 13:15:29 +0200

    add of OrderManagementStepdefs.java

---
+++ src/test/java/fr/unice/polytech/app/OrderManagementStepdefs.java
@@ -0,0 +1,75 @@
+
+package fr.unice.polytech.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.util.List;
+
+public class OrderManagementStepdefs {
+
+    private List<String> restaurants;
+    private String selectedRestaurant;
+    private List<String> menuItems;
+    private List<String> cartItems;
+
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+        // Code to navigate to the login page
+    }
+
+    @When("I log in as {string}")
+    public void iLogInAs(String username) {
+        // Code to log in as the given user
+    }
+
+    @Then("I should be logged in as {string}")
+    public void iShouldBeLoggedInAs(String username) {
+        // Code to verify the logged-in user
+    }
+
+    @When("I view the list of restaurants")
+    public void iViewTheListOfRestaurants() {
+        restaurants = List.of("Le Délice", "Pizza Palace", "Burger Barn"); // Sample list of restaurants
+    }
+
+    @Then("I should see the list of available restaurants")
+    public void iShouldSeeTheListOfAvailableRestaurants() {
+        // Code to verify the list of restaurants
+    }
+
+    @When("I select the restaurant {string}")
+    public void iSelectTheRestaurant(String restaurantName) {
+        selectedRestaurant = restaurantName;
+        if ("Le Délice".equals(restaurantName)) {
+            menuItems = List.of("Pizza Margherita", "Soda", "Salad"); // Sample menu for "Le Délice"
+        }
+        // Add code for other restaurants' menus if needed
+    }
+
+    @Then("I should see the menu of the restaurant {string}")
+    public void iShouldSeeTheMenuOfTheRestaurant(String restaurantName) {
+        // Code to verify the menu items of the selected restaurant
+    }
+
+    @When("I add the item {string} with a quantity of {int} to my cart")
+    public void iAddTheItemWithAQuantityToMyCart(String itemName, int quantity) {
+        // Code to add the item to the cart
+        cartItems.add(itemName); // Adding to the sample cart items list
+    }
+
+    @Then("my cart should contain the item {string} with a quantity of {int}")
+    public void myCartShouldContainTheItemWithAQuantityOf(String itemName, int quantity) {
+        // Code to verify the cart contents
+    }
+
+    @When("I validate my cart")
+    public void iValidateMyCart() {
+        // Code to validate the cart and create an order
+    }
+
+    @Then("an order should be created with the items from my cart")
+    public void anOrderShouldBeCreatedWithTheItemsFromMyCart() {
+        // Code to verify the created order
+    }
+}
