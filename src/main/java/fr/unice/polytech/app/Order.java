8b20ec94812af9ad31dd230eb987e0e9fcfadb80
ChatelainClement <clementchatelain@hotmail.fr>
2023-10-24 14:54:47 +0200

    Ajout de tests pour vérifier l'état d'une commande en vue d'un refactor pour les ajouter aux tests OrderManagement #3

--- src/main/java/fr/unice/polytech/app/Order.java
+++ src/main/java/fr/unice/polytech/app/Order.java
@@ -9,7 +9,6 @@ public class Order {
     private List<Item> items;
     private String clientAddress;
 
-
     private LocalTime placedTime;
 
     private UUID id;
@@ -67,5 +66,58 @@ public class Order {
      }
     public LocalTime getDeliveryTime() {
         return deliveryTime;
+    }
+
+    public void pay() {
+        status = OrderStatus.Paid;
+    }
+
+    public void accept() {
+        status = OrderStatus.Accepted;
+        acceptedTime = LocalTime.now();
+    }
+
+    public void pickUp() {
+        status = OrderStatus.PickedUp;
+    }
+
+    public void deliver() {
+        status = OrderStatus.Delivered;
+    }
+
+    public void validate() {
+        status = OrderStatus.Ready;
+    }
+
+    public void reject() {
+        status = OrderStatus.Cancelled;
+    }
+
+    public boolean isRejected() {
+        return status == OrderStatus.Cancelled;
+    }
+
+    public boolean isAccepted() {
+        return status == OrderStatus.Accepted;
+    }
+
+    public boolean isPickedUp() {
+        return status == OrderStatus.PickedUp;
+    }
+    public boolean isDelivered() {
+        return status == OrderStatus.Delivered;
+    }
+    public boolean isPlaced() {
+        return status == OrderStatus.Placed;
+    }
+    public boolean isPaid() {
+        return status == OrderStatus.Paid;
+    }
+    public boolean isReady() {
+        return status == OrderStatus.Ready;
+    }
+
+    public boolean isClosed() {
+        return status == OrderStatus.Cancelled || status == OrderStatus.Delivered;
     }
 }
