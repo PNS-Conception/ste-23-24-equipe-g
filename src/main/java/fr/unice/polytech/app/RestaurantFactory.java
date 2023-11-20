b100d3d2a134e02f8877f98281d64a0f8b711c1b
sikakaleb <dassou-ablam-kaleb.sika@etu.univ-cotedazur.fr>
2023-11-20 23:14:21 +0100

    remove restaurant factory #7

--- src/main/java/fr/unice/polytech/app/RestaurantFactory.java
+++
@@ -1,5 +0,0 @@
-package fr.unice.polytech.app;
-
-public interface RestaurantFactory {
-    Restaurant createRestaurant(String name, Menu menu, RestaurantType type);
-}
--- src/main/java/fr/unice/polytech/app/RestaurantService.java
+++ src/main/java/fr/unice/polytech/app/RestaurantService.java
@@ -4,7 +4,6 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 
 import static fr.unice.polytech.app.Day.getAllDayOfWeeks;
@@ -16,11 +15,6 @@ public class RestaurantService {
     private CapacityManager capacityManager;
     private RestaurantRepository restaurantRepository; // Assuming there's a repository for data access
 
-    /*public RestaurantService(RestaurantRepository restaurantRepository) {
-        this.restaurants = new HashMap<>();
-        this.restaurantRepository = restaurantRepository;
-        capacityManager = CapacityManager.getInstance();
-    }*/
     public RestaurantService() {
         capacityManager = CapacityManager.getInstance();
         this.restaurants = new HashMap<>();
@@ -36,29 +30,11 @@ public class RestaurantService {
         if (restaurants.containsKey(name)) {
             throw new IllegalArgumentException("Restaurant already exists with this name: " + name);
         }
-
         Restaurant newRestaurant = new Restaurant(name, menu);
         restaurants.put(name, newRestaurant);
-        initializeDefaultCapacity(newRestaurant);
+        initializeDefaultCapacity(newRestaurant); // Supposons que cette méthode existe pour initialiser la capacité
         return newRestaurant;
     }
-
-    /*public void addRestaurant(Restaurant restaurant) {
-        restaurants.put(restaurant.getName(), restaurant);
-        initializeDefaultCapacity(restaurant);
-    }*/
-    /*private void initializeDefaultCapacity(Restaurant restaurant) {
-        LocalDate today = LocalDate.now();
-        for (DayOfWeek day : EnumSet.allOf(DayOfWeek.class)) {
-            LocalDate nextDay = today.with(day); // Définir le jour de la semaine
-            for (int hour = 0; hour < 24; hour++) {
-                LocalDateTime dateTime = LocalDateTime.of(nextDay, LocalTime.of(hour, 0));
-                capacityManager.setCapacity(restaurant, dateTime, CapacityManager.DEFAULT_CAPACITY_PER_HOUR);
-            }
-        }
-    }*/
-
-
     private List<Shift> getDefaultShifts() {
         List<Shift> defaultShifts = new ArrayList<>();
         for (Day day : getAllDayOfWeeks()) {
@@ -69,7 +45,6 @@ public class RestaurantService {
 
 
     private void initializeDefaultCapacity(Restaurant restaurant) {
-        boolean res =restaurant.getSchedule().isEmpty();
         List<Shift> shifts = restaurant.getSchedule().isEmpty() ? getDefaultShifts() : restaurant.getSchedule();
 
         LocalDate startDate = LocalDate.now().withDayOfMonth(1); // Début du mois
@@ -96,20 +71,17 @@ public class RestaurantService {
         // Return a pre-populated list of mocked restaurants
         // This is just for testing; in a real app, you'd query the database
         if (this.restaurantRepository == null) { // Check if repository is mocked
-            // Return mocked data
             return Arrays.asList(
                     new Restaurant("Pizza Place", new Menu(Arrays.asList(
                             new Dish("Margherita", Arrays.asList("Tomato", "Mozzarella", "Basil"), 7.99),
                             new Dish("Pepperoni", Arrays.asList("Tomato", "Mozzarella", "Pepperoni"), 8.99)
                     ))),
                     new Restaurant("Burger Bar", new Menu(Arrays.asList(
-                            new Dish("Classic Burger", Arrays.asList("Beef", "Lettuce", "Tomato", "Cheese"), 6.99),
+                            new Dish("Classic Burger", Arrays.asList("Beef", "Lettuce", "Tomto", "Cheese"), 6.99),
                             new Dish("Veggie Burger", Arrays.asList("Black Bean Patty", "Lettuce", "Tomato", "Cheese"), 5.99)
                     )))
             );
         } else {
-            // In production, you'd do something like:
-            // return restaurantRepository.findAll();
             return new ArrayList<>();
         }
     }
@@ -118,7 +90,4 @@ public class RestaurantService {
         return restaurants.get(name);
     }
 
-    // Additional methods related to restaurant operations can be added here
-    // For example, finding a restaurant by ID, updating restaurant details, etc.
 }
-
